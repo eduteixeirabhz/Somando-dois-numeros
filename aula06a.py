adcca7566d96fb3322459625c7ea8ebd5fb0031f
eduteixeirabhz <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-01 16:20:23 -0300

    Add files via upload

---
+++ aula06a.py
@@ -0,0 +1,5 @@
+n1 = int(input('digite um valor: '))
+n2 = int(input('digite outro: '))
+s = n1 + n2
+#print('A soma entre', n1, 'e', n2, 'vale: ', s) (ANTIGO MÉTODO)
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
